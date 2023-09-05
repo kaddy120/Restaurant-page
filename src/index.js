cbb2974fe72a9a7c9405d0f84906fbf7fde52622
kaddy120 <kaddy120@gmail.com>
2023-09-05 20:18:12 +0200

    20:18

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ function App() {
       <li>${Link('/menu', 'Menu')}</li>
       <li>${Link('/contact', 'Contact')}</li>
       <li>${Link('/menu', 'Menu')}</li>
-
       <li class='list-space'></li>
       ${socialsList}
     </ul>
